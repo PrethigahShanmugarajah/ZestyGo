d65a810d61bce74489d0a2691d16ff6bc9bba8ec
PrethigahShanmugarajah <prethigah@gmail.com>
2025-09-30 21:50:32 +0530

    Update server.js and configure .gitignore; ignore frontend and backend .env and node_modules

---
+++ Backend/config/db.js
@@ -0,0 +1,15 @@
+// Backend/config/db.js
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Connection Failed:", error);
+    process.exit(1);
+  }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,21 @@
+// Backend/server.js
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 /* ---------------- App Config ---------------- */
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
-/* ---------------- Middleare ---------------- */
+/* ---------------- Middleware ---------------- */
 app.use(express.json());
 app.use(cors());
+
+/* ---------------- Database Connected ---------------- */
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API IS WORKING");
