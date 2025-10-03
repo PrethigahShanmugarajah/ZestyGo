ba09e5140f1a1ea1ba044d261e7b29b8ff338db6
PrethigahShanmugarajah <prethigah@gmail.com>
2025-10-03 20:54:52 +0530

    Update frontend, backend, and admin code

---
+++ Backend/controllers/cartController.js
@@ -0,0 +1,11 @@
+// Backend / controllers / cartController.js
+import userModel from "../models/userModel.js";
+
+/* ---------------- Add Food Item to User Cart ---------------- */
+export const addToCart = async (req, res) => {};
+
+/* ---------------- Remove Food Item from User Cart ---------------- */
+export const removeFromCart = async (req, res) => {};
+
+/* ---------------- Fetch  User Cart Data ---------------- */
+export const getCart = async (req, res) => {};
