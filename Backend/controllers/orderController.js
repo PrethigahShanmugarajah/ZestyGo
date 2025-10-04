3469a20a9532944bbca7239e5cb82d101636ee82
PrethigahShanmugarajah <prethigah@gmail.com>
2025-10-04 21:23:42 +0530

    Initialize order feature files (controller, model, route) and update server

---
+++ Backend/controllers/orderController.js
@@ -0,0 +1,7 @@
+// Backend / controllers / orderController.js
+import userModel from "../models/userModel.js";
+import orderModel from "../models/orderModel.js";
+import stripe from "stripe";
+
+/* ---------------- PLACING USER ORDER FOR FRONTEND ---------------- */
+export const placeOrder = async (req, res) => {};
--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-// Backend/server.js
 // Backend / server.js
 import express from "express";
 import cors from "cors";
@@ -10,6 +9,7 @@ import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 dotenv.config();
 
@@ -32,6 +32,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static(path.join(__dirname, "uploads")));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API IS WORKING");
