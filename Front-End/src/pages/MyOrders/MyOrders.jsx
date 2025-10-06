f0056b4e9695b29b7522507eec592b60137c13fb
PrethigahShanmugarajah <prethigah@gmail.com>
2025-10-06 21:19:36 +0530

    Add MyOrders page and update App routing

---
+++ Front-End/src/pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,55 @@
+// Front-End / src / pages / MyOrders / MyOrders.jsx
+import React from "react";
+import "./MyOrders.css";
+import { useState } from "react";
+import { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
+import { useEffect } from "react";
+import { assets } from "../../assets/assets";
+
+const MyOrders = () => {
+  const { url, token } = useContext(StoreContext);
+  const [data, setData] = useState([]);
+
+  const fetchOrders = async () => {
+    const response = await axios.post(
+      url + "/api/order/userorders",
+      {},
+      { headers: token }
+    );
+    setData(response.data.data);
+    // console.log(response.data.data);
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchOrders;
+    }
+  }, [token]);
+
+  return (
+    <div className="my-orders">
+      <h2>My Ordrs</h2>
+      <div className="container">
+        {data.map((order, index) => {
+          return (
+            <div key={index} className="my-orders-order">
+              <img src={assets.parcel_icon} alt="" />
+              <p>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + "X" + item.quanity;
+                  } else {
+                  }
+                })}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MyOrders;
